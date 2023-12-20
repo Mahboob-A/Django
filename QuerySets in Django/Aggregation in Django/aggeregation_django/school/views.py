82f5bb2fc6ba87ef6a82d98177d64bca4b61f123
Mahboob-Alam <iammahboob.a@gmail.com>
2023-12-20 14:32:59 +0530

    add limit offset queryset

--- QuerySets in Django/Aggregation in Django/aggeregation_django/school/views.py
+++ QuerySets in Django/Aggregation in Django/aggeregation_django/school/views.py
@@ -8,7 +8,30 @@ from .models import Student
 
 def home(request): 
         
-        students = Student.objects.all()
+        ''' 
+        limiting queryset : if silicing has either stop or start and stop, then it returns a queryset. 
+        here, SQL LIMIT and OFFSET is used. 
+        
+        Example SQL Query : 
+        SELECT "school_student"."id", "school_student"."name", 
+        "school_student"."roll", "school_student"."grade", "school_student"."city", "school_student"."marks", 
+        "school_student"."admission_date", "school_student"."pass_date" 
+        FROM "school_student" LIMIT 4 OFFSET 3
+        '''
+        
+        students = Student.objects.all()[3:7]
+        
+        
+        
+        
+        ''' 
+        limiting queryset | the below start : end : step slicing returns a list, not queryset. so, this student is a list, so no ORM 
+        activiting would work here. 
+        '''
+        # students = Student.objects.all()[3:7:2]
+        
+        
+        print('student type: ', type(students))
 
         # aggregatoin classes 
         avg_marks = students.aggregate(Avg('marks'))
@@ -28,7 +51,9 @@ def home(request):
                 'total_marks_count' : total_marks_count
         }
 
-        print("Return : ", students)
+        
+        
+        print("Queryset : ", students)
         print()
         print("SQL Query: ", students.query)
         return render(request, 'school/home.html', context)
